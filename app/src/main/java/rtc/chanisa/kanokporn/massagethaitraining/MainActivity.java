098ab01afd156149fcc24507d4533c4d7d2be422
Home <tinchanisa@gmail.com>
2016-11-29 15:36:39 +0700

    Initial commit

---
+++ app/src/main/java/rtc/chanisa/kanokporn/massagethaitraining/MainActivity.java
@@ -0,0 +1,16 @@
+package rtc.chanisa.kanokporn.massagethaitraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Test By Master UNG
+    // test by fern
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
